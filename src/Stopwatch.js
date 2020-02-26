427c9f81f9f24ae24a79430b9c7eedab39aa10d3
Mateus Soares <mateussoaress1997@gmail.com>
2020-02-26 17:17:57 -0300

    Refactoring Stopwatch class

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,36 +1,46 @@
 var Stopwatch = function(options) {
-	this.options = options;
-	this.millisecondsCounter = 0;
-	this.interval = null;
+    
+    this.options = options;
+    
+    this.millisecondsCounter = 0;
+    
+    this.interval = null;
 	this.buildEventListeners();
+
 };
 
 Stopwatch.prototype.buildEventListeners = function(event) {
-	var self = this;
-	var options = self.options;
-	var startButton = document.getElementById(options.startButtonId);
-	var stopButton = document.getElementById(options.stopButtonId);
-	var resetButton = document.getElementById(options.resetButtonId);
+    
+    var self = this;
+
+    var startButton = document.getElementById(self.options.startButtonId);
+	var stopButton = document.getElementById(self.options.stopButtonId);
+	var resetButton = document.getElementById(self.options.resetButtonId);
 
 	startButton.addEventListener("click", function(event) {
 		self.start();
-	});
+    });
+    
 	stopButton.addEventListener("click", function(event) {
 		self.stop();
-	});
+    });
+    
 	resetButton.addEventListener("click", function(event) {
 		self.reset();
-	});
+    });
+    
 };
 
 Stopwatch.prototype.start = function(event) {
+
 	var self = this;
 
 	self.stop();
 	self.interval = setInterval(function() {
 		self.millisecondsCounter += 10;
 		self.update();
-	}, 10);
+    }, 10);
+    
 };
 
 Stopwatch.prototype.stop = function(event) {
@@ -45,16 +55,25 @@ Stopwatch.prototype.reset = function(event) {
 
 Stopwatch.prototype.update = function(event) {
 	var self = this;
-	var options = self.options;
 
-	var minutesDisplay = document.getElementById(options.minutesDisplayId);
-	var secondsDisplay = document.getElementById(options.secondsDisplayId);
+	var minutesDisplay = document.getElementById(
+        self.options.minutesDisplayId
+    );
+	var secondsDisplay = document.getElementById(
+        self.options.secondsDisplayId
+    );
 	var millisecondsDisplay = document.getElementById(
-		options.millisecondsDisplayId
+		self.options.millisecondsDisplayId
 	);
 
-	var minutes = Math.floor(self.millisecondsCounter / 60000);
-	var seconds = Math.floor((self.millisecondsCounter % 60000) / 1000);
+	var minutes = Math.floor(
+        self.millisecondsCounter / 60000
+    );
+
+	var seconds = Math.floor(
+        (self.millisecondsCounter % 60000) / 1000
+    );
+    
 	var milliseconds = Math.floor(
 		((self.millisecondsCounter % 60000) % 1000) / 10
 	);
